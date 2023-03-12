2d96ecf8a997e31cdae21ace7cea4265dd36a9b1
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-13 03:14:58 +0530

    Feat : SignOut Function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { auth, provider } from '../config/firebase'
 import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate = useNavigate()
 
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider)
     console.log(result)
 
-    return 
+    navigate('/') 
   }
 
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom' 
+import { auth } from '../config/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { signOut } from 'firebase/auth'
 
 const Navbar = () => {
+
+  const [user] = useAuthState(auth)
+
+  const signUserOut = async () => {
+    await signOut(auth)
+  }
+ 
+
   return (
     <div>
       <Link to="/"> Home </Link>
       <Link to="/login">Login</Link>
+
+      {user && (
+        <>
+          <div>
+            <p>{user?.displayName}</p>
+          </div>
+
+          <button onClick={signUserOut}>Logout</button>
+        </>
+      )}
     </div>
   )
 }
