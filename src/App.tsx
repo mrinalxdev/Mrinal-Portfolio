055b36457f103c27654e3ffbe8436f6714fe0cb8
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-12 21:45:53 +0530

    Added New Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import Login from './components/Login'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-2xl">Hello World</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <HomePage/>
+        } />
+        <Route path="login" element={
+          <Login/>
+        } />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="text-center">
+        Login
+    </div>
+  )
+}
+
+export default Login
