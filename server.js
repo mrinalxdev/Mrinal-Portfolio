463b384abf691f0696100fda55fd38b0cebf70fc
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-16 18:58:53 +0530

    Added Data

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+const multer = require("multer")
 const express = require("express")
 const app = express()
+
+const upload = multer({ dest: "uploads" })
 
 app.set("view engine", "ejs")
 
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.post("/upload", (req, res) => {
+    res.send("hi")
 })
 
 app.listen(3000)
