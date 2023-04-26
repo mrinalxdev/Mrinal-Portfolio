5ecd734f62d0fe95495d8501feab0b39713cfbcf
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-27 02:19:10 +0530

    Working in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
+import { useState } from "react"
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false)
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo" />
+          <img src={""} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="nav-toggler-icon"></span>
+          <span className="nav-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -19,6 +22,22 @@ export const NavBar = () => {
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#blogs">Blogs</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log("Connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
