ed0be003af86a5d23f41769e655e729ad12bb225
Mrinal <mrinalbitsat@gmail.com>
2023-05-02 23:06:05 +0530

    Added New Data

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <h1>Hello Mrinal's Portfolio</h1>
+      <h1 className="text-2xl">Hello Mrinal's Portfolio</h1>
     </main>
   )
 }
