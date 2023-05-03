093d06ae2d4b4a2a90d085825432bbc1e48e0a24
Mrinal <mrinalbitsat@gmail.com>
2023-05-03 10:40:00 +0530

    Added New Data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl">Hello Mrinal's Portfolio</h1>
-    </main>
-  )
+    <>
+      <Head>
+        <title>Mrinal Portfolio</title>
+      </Head>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-2xl">Mrinal</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer"/>
+              </li>
+              <li>
+                <a href="#" className="bg-gradient-to-r from-cyan-500 to-teal-500  text-white px-6 py-2 rounded-md ml-8">Resume</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
+    </>
+  );
 }
