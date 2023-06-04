b7cdecc00284ea1d3a3557508013e6710417d7ef
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-04 11:50:47 +0530

    Added New Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
-import './App.css';
 
 function App() {
-  return (
-    <div>
-      Hello this is my portfolio
-    </div>
-  );
+  return <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { ReactNavbar } from 'overlay-navbar'
+
+const Header = () => {
+  return (
+    <ReactNavbar 
+      navColor1 = "white"
+      navColor2 = "hsl(219, 48%, 8%)"
+      burgerColor = "hsl(250, 100%, 75%)"
+    
+    />
+  )
+}
+
+export default Header
