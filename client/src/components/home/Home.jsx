a82dbb9775793d962fbfe4940e3868d5f7bd66f7
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-08 01:58:46 +0530

    Added new data

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,21 +1,44 @@
 import React, { useEffect } from "react";
-import THREE from "three"
-import "./Home.css"
+import * as THREE from "three";
+import "./Home.css";
 
 const Home = () => {
-
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.Prespective(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new 
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+    });
 
-  }, [])
+    const geometery = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-  return <div className="home">
-    <canvas className="homeCanvas">
+    const mesh = new THREE.Mesh(geometery, material);
 
-    </canvas>
-  </div>;
+    scene.add(mesh);
+
+    camera.position.z = 5;
+    const animate = () => {
+      requestAnimationFrame(animation)
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    }
+
+    animate()
+
+  }, []);
+
+  return (
+    <div className="home">
+      <canvas className="homeCanvas"></canvas>
+    </div>
+  );
 };
 
 export default Home;
