f62b6f0c3dfb9faf213dbabe820637d21ee6aa53
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-08 00:31:13 +0530

    Added New Data

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import THREE from "three"
+import "./Home.css"
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.Prespective(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new 
+
+  }, [])
+
+  return <div className="home">
+    <canvas className="homeCanvas">
+
+    </canvas>
+  </div>;
+};
+
+export default Home;
