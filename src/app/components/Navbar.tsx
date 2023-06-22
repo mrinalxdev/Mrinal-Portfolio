fe9daef9d7f8749bb2e7f6d919a6dd18aa46ecbc
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-22 17:17:16 +0530

    Added New Data

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-2">
-      <div>
-        <div />
-        <span>Portfolio</span>
+      <div className="flex items-center gap-[1ch]">
+        <div className="w-7 h-7 bg-yellow-400 rounded-full" />
+        <span className="text-sm font-semibold text-white">Me</span>
       </div>
-      <div>
-        <Link href="#">Home</Link>
+      <div className="flex gap-12 text-[16px] pr-1 text-white">
+        <Link href="#" className="text-white font-medium">Home</Link>
         <Link href="#">Blogs</Link>
         <Link href="#">Contact</Link>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,29 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-    </main>
+    <>
+      <main>
+        <Navbar />
+      </main>
+      <section className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-4 mb-4">
+        <div className="h-[30rem] rounded-3xl p-10 flex flex-col gap-16 bg-[url(../../public/gradient-bg.jpg)] bg-cover">
+          <h1 className="text-4xl font-semibold">
+            Hello, I am a Full stack developer with 2 years of experience
+          </h1>
+          <p className="flex-1">
+            A seasoned full stack developer with expertise in JavaScript, React,
+            Node, Ruby on Rails, Express, MongoDB, Python, and Typescript. With
+            a strong passion for creating robust and scalable web applications,
+            I bring a wealth of knowledge and experience to every project.{" "}
+          </p>
+          <div className="flex flex-col lg:flex-row items-center gap-4 justify-self-end">
+            <button className="text-white bg-[#0A2239] font-medium py-3 px-12 rounded-full">Contact Me</button>
+            <div>{/* Social Links */}</div>
+          </div>
+        </div>
+
+        <div />
+      </section>
+    </>
   );
 }
